f44a76d765c8baeb2cbe79131f521fba4afa2178
Alexey Martushev <a.martushev@ledas.com>
2025-06-18 23:55:11 +0700

    Separate tests

--- GTests/AssertTests.cpp
+++ GTests/AssertTests.cpp
@@ -1,14 +1,6 @@
 #include <gtest/gtest.h>
 #include <Graph.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-	// Expect two strings not to be equal.
-	EXPECT_STRNE("hello", "world");
-	// Expect equality.
-	EXPECT_EQ(7 * 6, 42);
-}
-
 TEST(AssertTest, AddEdge)
 {
 	Graph graph(3);
