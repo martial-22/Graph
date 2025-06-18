ca2f098c39207c0c97115c3eca2f0883ce795b02
Alexey Martushev <a.martushev@ledas.com>
2025-06-18 19:06:17 +0700

    Add unit tests

--- GTests/tests.cpp
+++ GTests/tests.cpp
@@ -16,3 +16,27 @@ TEST(AssertTest, AddEdge)
 	EXPECT_NO_THROW(graph.addEdge(1, 2));
 	EXPECT_THROW(graph.addEdge(4, 2), std::runtime_error);
 }
+
+TEST(AssertTest, GetEdge)
+{
+	Graph graph(3);
+
+	Graph::IndexT edgeIndex = graph.addEdge(1, 2);
+	EXPECT_EQ(edgeIndex, 0);
+	EXPECT_NO_THROW(graph.edge(0));
+
+	EXPECT_THROW(graph.edge(1), std::runtime_error);
+	EXPECT_THROW(graph.edge(2), std::runtime_error);
+}
+
+TEST(AssertTest, GetAdjacencyList)
+{
+	Graph graph(2);
+	EXPECT_NO_THROW(graph.addEdge(0, 1));
+
+	EXPECT_NO_THROW(graph.adjacencyList(0));
+	EXPECT_NO_THROW(graph.adjacencyList(1));
+
+	EXPECT_THROW(graph.adjacencyList(2), std::runtime_error);
+	EXPECT_THROW(graph.adjacencyList(3), std::runtime_error);
+}
